3f956b1c359843c196ec0bf75a438ca058ebb83c
KD Leaker <shakibzeidi0@gmail.com>
2026-08-20 23:19:13 +0330

    Migrate DB and switched to pgsql
    + Migrate DB migrations
    + Swith DB to PostgreSQL

--- mysite/settings.py
+++ mysite/settings.py
@@ -75,9 +75,13 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 # https://docs.djangoproject.com/en/6.1/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": "django_tutorial",
+        "USER": "postgres",
+        "PASSWORD": "sh138256",
+        "HOST": "127.0.0.1",
+        "PORT": "5432",
     }
 }
 
